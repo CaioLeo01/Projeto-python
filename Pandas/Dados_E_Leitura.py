64758de2c6488533bc93515efd36a5cd88b4d9d0
CaioLeo01 <caio.m.c.leonardo@gmail.com>
2024-06-19 16:28:38 -0300

    Update

--- Pandas/Dados_E_Leitura.py
+++ Pandas/Dados_E_Leitura.py
@@ -8,14 +8,7 @@ class Dados_Leitura:
         self._processar_dados()
 
     def _processar_dados(self):
-        try:
-            tabela = pd.read_excel(self.file_path)
-        except FileNotFoundError:
-            print(f"Arquivo '{self.file_path}' não encontrado. Verifique o caminho e tente novamente.")
-            exit()
-        except Exception as e:
-            print(f"Ocorreu um erro ao ler o arquivo '{self.file_path}': {str(e)}")
-            exit()
+        tabela = pd.read_excel(self.file_path)
         
         self.contagem_bichos = tabela['Bicho'].value_counts()
         self.media_aparicoes = self.contagem_bichos.mean()
--- Pandas/GraficoJogoDoBicho.py
+++ Pandas/GraficoJogoDoBicho.py
@@ -1,6 +1,5 @@
-import pandas as pd
 import matplotlib.pyplot as plt
-from Pandas.Dados_E_Leitura import Dados_Leitura  
+from Dados_E_Leitura import Dados_Leitura
 
 class GraficoJogoDoBicho:
     def __init__(self, dados_jogo_do_bicho):
@@ -9,8 +8,11 @@ class GraficoJogoDoBicho:
 
     def plot_grafico(self):
         import numpy as np
+        
         np.random.seed(42)
         animais_aleatorios = self.dados.contagem_bichos.sample(frac=1)
+
+        total_amostras = len(animais_aleatorios)  # Total de amostras (jogos) consideradas
 
         plt.figure(figsize=(12, 6))
         bars = plt.bar(animais_aleatorios.index, animais_aleatorios.values, color='skyblue')
@@ -20,7 +22,7 @@ class GraficoJogoDoBicho:
             if bar.get_height() in maiores_medias.values:
                 bar.set_color('red')
 
-        plt.xlabel(f'Média: {self.dados.media_aparicoes:.2f}', ha='center', fontsize=20, color='green')
+        plt.xlabel(f'Média: {self.dados.media_aparicoes:.2f}\n Dados de 1º a 17º de junho ', ha='center', fontsize=20, color='green')
         plt.ylabel('Número de Aparições')
         plt.title('Número de Aparições por Animal')
         plt.xticks(rotation=45)
@@ -30,11 +32,19 @@ class GraficoJogoDoBicho:
 
         plt.axhline(y=self.dados.media_aparicoes, color='green', linestyle='--', linewidth=1.0)
 
+        
+        probabilidades = self.dados.contagem_bichos / total_amostras
+        for idx, value in enumerate(animais_aleatorios.index):
+            percentual = probabilidades.get(value, 0) * 100  
+            percentual = percentual / 10 
+            plt.text(idx, animais_aleatorios[value] + 1 + 1, f'{percentual:.2f}%', ha='center', va='bottom', fontsize=8, color='gray')
+
+
         plt.tight_layout()
         plt.show()
 
 if __name__ == '__main__':
     file_path = r'C:\Users\Caio Manoel\Desktop\Facul\Pandas\JogoDoBicho.xlsx'
 
-    dados = Dados_Leitura(file_path)  # Criando o objeto dados utilizando Dados_Leitura
-    GraficoJogoDoBicho(dados)  # Passando o objeto dados para a classe GraficoJogoDoBicho
+    dados = Dados_Leitura(file_path)
+    GraficoJogoDoBicho(dados)
